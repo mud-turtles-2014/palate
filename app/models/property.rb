d218d08fca23f2d9694587679f2c7612039e522a
Katherine & steph t. zhang <pair+kdmcclin.stephtzhang@devbootcamp.com>
2014-10-17 19:36:27 -0400

    Add options association

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,5 @@ class Property < ActiveRecord::Base
   has_many :wine_properties
   has_many :wines, through: :wine_properties
   has_many :user_answers, through: :wine_properties
+  has_many :options
 end
