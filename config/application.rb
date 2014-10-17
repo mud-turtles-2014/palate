5b97d8889eff051171af6a6c75361d994debb167
Katherine <kdmcclin@gmail.com>
2014-10-17 10:53:00 -0400

    Update time info

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,9 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module WineTasting
-  class Application < Rails::Application
+class Application < Rails::Application
+    config.time_zone = 'Eastern Time (US & Canada)'
+end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -26,5 +27,3 @@ module WineTasting
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-  end
-end
