a1a1f1a5e41d2f1b1616aea59610c076e78afeea
J Armstrong <jssc29@gmail.com>
2014-10-16 21:53:37 -0400

    Remove unnecessary root path

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # root 'sessions#new'
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
 
