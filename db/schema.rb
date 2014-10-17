db731a9f1ff18950785bbb7fd9fa8e7048a8bfca
steph t. zhang <stephtzhang@gmail.com>
2014-10-17 15:08:36 -0400

    Add more question seeds and fix user seed password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016230247) do
+ActiveRecord::Schema.define(version: 20141017190037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,9 +37,9 @@ ActiveRecord::Schema.define(version: 20141016230247) do
 
   create_table "questions", force: true do |t|
     t.string   "question"
-    t.string   "answer_data_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "red_only"
   end
 
   create_table "user_answers", force: true do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,21 @@ EventWine.create!([
   {is_attending: nil, event_id: 1, wine_id: 1, user_id: 1}
 ])
 Question.create!([
-  {question: "fruits", answer_data_type: nil},
-  {question: "mineral", answer_data_type: nil},
-  {question: "mineral_level", answer_data_type: nil},
-  {question: "oak", answer_data_type: nil},
-  {question: "dry", answer_data_type: nil},
-  {question: "acid", answer_data_type: nil},
-  {question: "tannin", answer_data_type: nil},
-  {question: "alcohol", answer_data_type: nil}
+  {question: "fruits"}
+  {question: "mineral"},
+  {question: "mineral_level"},
+  {question: "oak"},
+  {question: "dry"},
+  {question: "acid"},
+  {question: "tannin", red_only: true},
+  {question: "alcohol"},
+  {question: "cool_climate"},
+  {question: "old_world"},
+  {question: "country"},
+  {question: "grape"},
 ])
 User.create!([
-  {name: "steph", email: "stephtzhang@gmail.com", password_digest: "$2a$10$bPe9.haPI5uWfGk5Mk.ja.Sbw1zjy3XxTZRlocfJsLDOb24t5IlmW"}
+  {name: "steph", email: "stephtzhang@gmail.com", password: "test", password_confirmation: "test"}
 ])
 Wine.create!([
   {name: "French Gamay", color: "red", description: "Gamay gets a bad rap, but Cru Beaujolais - from one of the 10 designated cru areas in the region - shows why the grape gets called the poor man's Pinot."}
@@ -28,5 +32,9 @@ WineQuestion.create!([
   {correct_answer: "true", question_id: 5, wine_id: 1},
   {correct_answer: "2", question_id: 6, wine_id: 1},
   {correct_answer: "4", question_id: 7, wine_id: 1},
-  {correct_answer: "4", question_id: 8, wine_id: 1}
+  {correct_answer: "4", question_id: 8, wine_id: 1},
+  {correct_answer: "true", question_id: 9, wine_id: 1}
+  {correct_answer: "true", question_id: 10, wine_id: 1}
+  {correct_answer: "France", question_id: 11, wine_id: 1}
+  {correct_answer: "Gamay", question_id: 12, wine_id: 1}
 ])
