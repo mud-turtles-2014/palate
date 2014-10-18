e2e37ab105ecaa8cad4766a97d9b5df8307a0884
Katherine & steph t. zhang <pair+kdmcclin.stephtzhang@devbootcamp.com>
2014-10-18 13:22:49 -0400

    Add fruit_condition enum

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -4,6 +4,7 @@ class Tasting < ActiveRecord::Base
 
   enum red_fruits: { red: 1, blue: 2, black: 3}
   enum white_fruits: { apple_pear: 1, stone: 2, citrus: 3, tropical: 4}
+  enum fruit_condition: { tart: 1, ripe: 2, over_ripe: 3, jammy: 4 }
   enum climate: { cool: 1, warm: 2}
   enum country: { france: 1, italy: 2, us: 3, australia: 4, argentina: 5, germany: 6, new_zealand: 7 }
   enum red_grape: { gamay: 1, cabernet_sauvignon: 2, merlot: 3, malbec: 4, syrah_shiraz: 5, pinot_noir: 6, sangiovese: 7, nebbiolo: 8, zinfandel: 9 }
