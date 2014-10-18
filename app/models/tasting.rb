2d7ea6d84ed3ff1cdf54acb6c8ef904a261d73a2
Katherine & steph t. zhang <pair+kdmcclin.stephtzhang@devbootcamp.com>
2014-10-17 22:51:34 -0400

    Adjust enum values

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -2,10 +2,10 @@ class Tasting < ActiveRecord::Base
   belongs_to :user
   belongs_to :wine
 
-  enum red_fruits: { red: 0, blue: 1, black: 2}
-  enum white_fruits: { apple_pear: 0, stone: 1, citrus: 2, tropical: 3}
-  enum climate: { cool: 0, warm:1}
-  enum country: { france: 0, italy: 1, us: 2, australia: 3, argentina: 4, germany: 5, new_zealand: 6}
-  enum red_grape: { gamay: 0, cabernet_sauvignon: 1, merlot: 2, malbec: 3, syrah_shiraz: 4 }
-  enum white_grape: { chardonnay: 0, sauvignon_blanc: 1, reisling: 2, chenin_blanc: 3, viognier: 4 }
+  enum red_fruits: { red: 1, blue: 2, black: 3}
+  enum white_fruits: { apple_pear: 1, stone: 2, citrus: 3, tropical: 4}
+  enum climate: { cool: 1, warm: 2}
+  enum country: { france: 1, italy: 2, us: 3, australia: 4, argentina: 5, germany: 6, new_zealand: 7 }
+  enum red_grape: { gamay: 1, cabernet_sauvignon: 2, merlot: 3, malbec: 4, syrah_shiraz: 5, pinot_noir: 6, sangiovese: 7, brunello: 8 }
+  enum white_grape: { chardonnay: 1, sauvignon_blanc: 2, riesling: 3, chenin_blanc: 4, viognier: 5 }
 end
