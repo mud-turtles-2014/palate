34cb8dde3cfeb082194911aa3b7be19ca64839ea
Katherine & steph t. zhang <pair+kdmcclin.stephtzhang@devbootcamp.com>
2014-10-18 12:51:21 -0400

    Creat event methods for attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,13 @@ class Event < ActiveRecord::Base
   validates :location, presence: true
   validates :date, presence: true
   validates :time, presence: true
+
+  # return array of user objs
+  def attending_users
+    self.event_wines.map {|event_wine| event_wine.wine_bringer }
+  end
+
+  def winelist
+    self.event_wines.map {|event_wine| event_wine.wine }
+  end
 end
