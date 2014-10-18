2925367fda6d1ac45f62963aa1b404671d61f361
J Armstrong <jssc29@gmail.com>
2014-10-18 13:40:10 -0400

    Add get_event_wine before action

--- app/controllers/event_wines_controller.rb
+++ app/controllers/event_wines_controller.rb
@@ -1,8 +1,15 @@
 class EventWinesController < ApplicationController
+  before_action :get_event_wine
 
 	def edit
   end
 
   def update
+  end
+
+  private
+
+  def get_event_wine
+    @event_wine = EventWine.find(params[:id])
   end
 end
