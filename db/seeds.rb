a95467e03f30d6d64ec7161169fa94ad8d50a523
steph t. zhang <stephtzhang@gmail.com>
2014-10-18 11:46:33 -0400

    Add first half of super user red tasting entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,6 +21,7 @@ Wine.create!([
   {name: "Argentinian Malbec", color: "red", description: "TKTKTK"},
   {name: "US Cabernet Sauvignon", color: "red", description: "TKTKTK"},
   {name: "US Merlot", color: "red", description: "TKTKTK"},
+  # note to steph: tastings inputted up to here
   {name: "US Zinfandel", color: "red", description: "TKTKTK"},
   {name: "French Cabernet Sauvignon", color: "red", description: "TKTKTK"},
   {name: "Italian Sangiovese", color: "red", description: "TKTKTK"},
@@ -32,6 +33,10 @@ Wine.create!([
 
 # seed pascaline's 'super user' tastings for grading
 Tasting.create!([
-  {user: User.first, wine: Wine.first, red_fruits: 'red', fruit_condition: 2, minerality: 4, oak: 2, dry: 1, acid: 2, tannin: 4, alcohol: 3, climate: 'cool', country: 'france', red_grape: 'gamay' },
-  {user: User.first, wine: Wine.first, red_fruits: 'red', fruit_condition: 2, minerality: 4, oak: 2, dry: 1, acid: 2, tannin: 4, alcohol: 3, climate: 'cool', country: 'france', red_grape: 'gamay' }
+  {user: User.first, wine: Wine.find_by(name: "French Gamay"), red_fruits: 'red', fruit_condition: 2, minerality: 4, oak: 2, dry: 1, acid: 2, tannin: 4, alcohol: 3, climate: 'cool', country: 'france', red_grape: 'gamay' },
+  {user: User.first, wine: Wine.find_by(name: "US Pinot Noir"), red_fruits: 'red', fruit_condition: 4, minerality: 3, oak: 4, dry: 2, acid: 3, tannin: 3, alcohol: 4, climate: 'warm', country: 'us', red_grape: 'pinot_noir' },
+  {user: User.first, wine: Wine.find_by(name: "Australian Shiraz"), red_fruits: 'red', fruit_condition: 5, minerality: 2, oak: 3, dry: 2, acid: 3, tannin: 3, alcohol: 5, climate: 'warm', country: 'australia', red_grape: 'syrah_shiraz' },
+  {user: User.first, wine: Wine.find_by(name: "Argentinian Malbec"), red_fruits: 'black', fruit_condition: 4, minerality: 2, oak: 4, dry: 2, acid: 3, tannin: 4, alcohol: 5, climate: 'warm', country: 'argentina', red_grape: 'malbec' },
+  {user: User.first, wine: Wine.find_by(name: "US Cabernet Sauvignon"), red_fruits: 'black', fruit_condition: 4, minerality: 2, oak: 5, dry: 2, acid: 3, tannin: 4, alcohol: 4, climate: 'warm', country: 'us', red_grape: 'cabernet_sauvignon' },
+  {user: User.first, wine: Wine.find_by(name: "US Merlot"), red_fruits: 'red', fruit_condition: 4, minerality: 2, oak: 5, dry: 2, acid: 3, tannin: 3, alcohol: 5, climate: 'warm', country: 'us', red_grape: 'merlot' },
   ])
