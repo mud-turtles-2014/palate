ae390d79d649aa82485d4bab957829cc2d0a6920
J Armstrong <jssc29@gmail.com>
2014-10-19 10:53:28 -0400

    Update mailer URLs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    @url = 'devbootcamp.com'
+    @url = 'http://pacific-retreat-6443.herokuapp.com'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 
   def invite_email(email, event_wine)
   	@email = email
   	@event_wine = event_wine
-  	@url  = 'localhost:3000/event_wines/' + @event_wine.id.to_s + '/edit'
+  	@url  = 'http://pacific-retreat-6443.herokuapp.com/event_wines/' + @event_wine.id.to_s + '/edit'
   	mail(to: @email, subject: 'Join us for a wine tasting!')
   end
 end
