e1fcbb278baf14c4ab00a8ac254e3a9d6b0e9487
steph t. zhang <stephtzhang@gmail.com>
2014-10-19 15:38:04 -0400

    Refactor current_tasting_attributes out of raw_score

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -18,27 +18,42 @@ class Tasting < ActiveRecord::Base
     super_tasting = super_tastings.find_by(event_wine: EventWine.find_by(wine: Wine.find_by(name: self.wine.name))) #current_wine))
 
     score = 0
-    color = self.wine.color
 
-    attributes = color == "white" ? white_tasting_attributes : red_tasting_attributes
+    attributes = current_tasting_attributes
 
-    attributes.each { |attribute| score += 1 if self.send(attribute) == super_tasting.send(attribute) }
+    attributes.each do |attribute|
+      score += 1 if self.send(attribute) == super_tasting.send(attribute)
+    end
 
     return score
   end
 
-  def get_tasting_attributes
+  def correct_categories
+  end
+
+  def incorrect_categories
+  end
+
+  def current_tasting_attributes
+    wine_color == "white" ? white_tasting_attributes : red_tasting_attributes
+  end
+
+  def wine_color
+    self.wine.color
+  end
+
+  def parse_tasting_attributes
     all_attributes = Tasting.last.attributes.map {|attribute, val| attribute}
     wine_attributes = all_attributes.reject {|attribute| /(_id|_at|\bid)/.match(attribute)}
     wine_attributes.map! {|attribute| attribute.to_sym}
   end
 
   def red_tasting_attributes
-    get_tasting_attributes.reject {|attribute| /(white_)/.match(attribute)}
+    parse_tasting_attributes.reject {|attribute| /(white_)/.match(attribute)}
   end
 
   def white_tasting_attributes
-    get_tasting_attributes.reject {|attribute| /(red_|tannin)/.match(attribute)}
+    parse_tasting_attributes.reject {|attribute| /(red_|tannin)/.match(attribute)}
   end
 end
 
