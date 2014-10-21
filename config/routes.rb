f298362eb50eae00e19c5966599e2b890e89973e
steph t. zhang <stephtzhang@gmail.com>
2014-10-20 21:29:57 -0400

    Add feedback show route

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
   get '/events/:id/my_results' => 'events#user_scores'
 
   get '/tasting_tutorial' => 'tutorials#tasting_tutorial'
+  get '/tasting/:id/feedback' => 'tastings#show_feedback'
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
