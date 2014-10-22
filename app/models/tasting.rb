1a4c452f737dcda64bd40099489768e279ba2ce8
steph t. zhang <stephtzhang@gmail.com>
2014-10-22 15:47:23 -0400

    Separate conclusion results into separate hash

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -33,17 +33,30 @@ class Tasting < ActiveRecord::Base
     user_results = {}
     correct_answers = {}
 
-    attributes.each do |attribute|
-      user_results[format_category(attribute)] = make_result_hash(attribute, self)
+    user_conclusions = {}
+    correct_conclusions = {}
 
-      correct_answers[format_category(attribute)] = make_result_hash(attribute, super_tasting)
+    attributes.each do |attribute|
+      if !conclusion_attr_array.include?(attribute)
+        user_results[format_category(attribute)] = make_result_hash(attribute, self)
+
+        correct_answers[format_category(attribute)] = make_result_hash(attribute, super_tasting)
+      else
+        user_conclusions[format_category(attribute)] = format_category(self.send(attribute))
+        correct_conclusions[format_category(attribute)] = format_category(super_tasting.send(attribute))
+      end
     end
 
     wine_bringer = self.event_wine.wine_bringer.name_or_email
     conclusion_score = is_reasonable_conclusion
     observation_score = is_reasonable_observation
 
-    return { user_results: user_results, correct_answers: correct_answers, wine_bringer: wine_bringer, conclusion_score: conclusion_score, observation_score: observation_score}
+    # take conclusions out of user_results and correct_answers
+    return { user_results: user_results, correct_answers: correct_answers, wine_bringer: wine_bringer, conclusion_score: conclusion_score, observation_score: observation_score, user_conclusions: user_conclusions, correct_conclusions: correct_conclusions}
+  end
+
+  def conclusion_attr_array
+    [:white_grape, :red_grape, :country, :climate]
   end
 
   def make_result_hash(attribute, tasting)
