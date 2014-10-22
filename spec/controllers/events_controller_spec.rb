7e1f71f608957c592fd164f0c6cc7e6b6d14cd0e
Katherine <kdmcclin@gmail.com>
2014-10-22 17:28:42 -0400

    Add test for redirect to root if not logged in

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -6,7 +6,7 @@ describe EventsController do
   end
 
   let(:user) {User.create(name: "pascaline", email: "boss@ladysomm.com", password: "test", password_confirmation: "test")}
-  let(:event) {Event.create(name: "Great Tasting", location: "48 Wall St, NY, NY", date: "2014-10-30 20:45:00", time: "2014-10-30 20:45:00", creator: user)}
+  let(:an_event) {Event.create(name: "Great Tasting", location: "48 Wall St, NY, NY", date: "2014-10-30 20:45:00", time: "2014-10-30 20:45:00", creator: user)}
 
   context 'GET #new' do
     it "assigns a new event to @event" do
@@ -23,13 +23,18 @@ describe EventsController do
   context 'GET #edit' do
     it "assigns the requested event to @event" do
       stub_current_user(user)
-      get :edit, id: event
-      expect(assigns(:event)).to eq event
+      get :edit, id: an_event
+      expect(assigns(:event)).to eq an_event
+    end
+
+    it "redirects with no valid user " do
+      get :edit, id: an_event
+      expect(response).to redirect_to root_path
     end
 
     it "renders the :edit template" do
-      event = :event
-      get :edit, id: event
+      stub_current_user(user)
+      get :edit, id: an_event
       expect(response).to render_template :edit
     end
   end
