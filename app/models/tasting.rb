6b088ecbe1daca1200b2b07e2cfb3488287e73ff
steph t. zhang <stephtzhang@gmail.com>
2014-10-21 22:40:41 -0400

    Remove tasting_notes from attributes to be scored

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -222,7 +222,7 @@ class Tasting < ActiveRecord::Base
 
   def parse_tasting_attributes
     all_attributes = Tasting.last.attributes.map {|attribute, val| attribute}
-    wine_attributes = all_attributes.reject {|attribute| /(_id|_at|\bid)/.match(attribute)}
+    wine_attributes = all_attributes.reject {|attribute| /(_id|_at|\bid|tasting_notes)/.match(attribute)}
     wine_attributes.map! {|attribute| attribute.to_sym}
   end
 
