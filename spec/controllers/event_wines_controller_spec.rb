0ebc4ec1ca971fa682632eeb7ec2325715462399
J Armstrong <jssc29@gmail.com>
2014-11-12 10:02:03 -0500

    Styling changed as per Hound CI recommendations

--- spec/controllers/event_wines_controller_spec.rb
+++ spec/controllers/event_wines_controller_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 describe EventWinesController do
 	def stub_current_user user
-    ApplicationController.any_instance.stub(:current_user) { user }
+  	ApplicationController.any_instance.stub(:current_user) { user }
   end
 
-  let(:user) {User.create(name: "jane doe", email: "jane@example.com", password: "test", password_confirmation: "test")}
-  let(:new_event_wine) {EventWine.create()}
+  let(:user) { User.create(name: "jane doe",
+  	email: "jane@example.com",
+  	password: "test",
+  	password_confirmation: "test") }
+  let(:new_event_wine) {EventWine.create}
 
-  context 'GET #edit' do
+  context "GET #edit" do
   	it "redirects with no valid user" do
     	new_event_wine
       get :edit, id: new_event_wine
@@ -30,14 +33,14 @@ describe EventWinesController do
     end
   end
 
-  describe 'PATCH #update' do
+  describe "PATCH #update" do
   	before :each do
-	    @event_wine = EventWine.create()
+	    @event_wine = EventWine.create
 		end
 
   	it "changes @event_wine's attributes " do
   		stub_current_user(user)
-  		patch :update, id: @event_wine, event_wine: {is_attending: true}
+  		patch :update, id: @event_wine, event_wine: { is_attending: true }
   		@event_wine.reload
   		expect(@event_wine.is_attending).to be true
   	end
@@ -45,7 +48,7 @@ describe EventWinesController do
   	context "user is attending event" do
 	  	it "redirects to show event_wine" do
 	  		stub_current_user(user)
-	  		patch :update, id: @event_wine, event_wine: {is_attending: true}
+	  		patch :update, id: @event_wine, event_wine: { is_attending: true }
 	  		expect(response).to redirect_to @event_wine
 	  	end
 	  end
@@ -53,7 +56,7 @@ describe EventWinesController do
 	  context "user is not attending event" do
 	  	it "redirects to homepage" do
 	  		stub_current_user(user)
-	  		patch :update, id: @event_wine, event_wine: {is_attending: false}
+	  		patch :update, id: @event_wine, event_wine: { is_attending: false }
 	  		expect(response).to redirect_to root_path
 	  	end
   	end
